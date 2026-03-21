7690e8a4cde496c56d4034627e6936e60b5cb15a
KabeloMashapa <mashapagseler03@gmail.com>
2026-03-21 11:41:28 +0200

    Initial Project commit

---
+++ src/test/java/com/kyc_ledger/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kyc_ledger.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
