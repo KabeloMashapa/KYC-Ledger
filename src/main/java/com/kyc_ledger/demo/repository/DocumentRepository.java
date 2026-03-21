e47a7e850b676aac27d317f9ba9c4aeac7fa2659
KabeloMashapa <mashapagseler03@gmail.com>
2026-03-21 19:59:22 +0200

    Adding DocumentRepository

--- src/main/java/com/kyc_ledger/demo/repository/DocumentRepository.java
+++ src/main/java/com/kyc_ledger/demo/repository/DocumentRepository.java
@@ -1,4 +1,11 @@
 package com.kyc_ledger.demo.repository;
+import com.kyc_ledger.demo.model.KycRecord;
+import com.kyc_ledger.demo.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.time.LocalDateTime;
+import java.util.Optional;
+public interface DocumentRepository extends JpaRepository<KycRecord,Long> {
 
-public class DocumentRepository {
+    Optional<KycRecord> findByUserRecord(String user);
 }
