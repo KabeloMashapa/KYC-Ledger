323a8180c9469cb6aa8e625231a8d4c2551de79b
KabeloMashapa <mashapagesler03@gmail.com>
2026-03-22 12:13:10 +0200

    Adding institutionRepositoty function

--- src/main/java/com/kyc_ledger/demo/repository/InstitutionRepository.java
+++ src/main/java/com/kyc_ledger/demo/repository/InstitutionRepository.java
@@ -1,4 +1,17 @@
 package com.kyc_ledger.demo.repository;
+import com.kyc_ledger.demo.model.Institution;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.util.List;
+import java.util.Optional;
 
-public class InstitutionRepository {
+@Repository
+public interface InstitutionRepository extends JpaRepository<Institution,Long> {
+    Optional<Institution> findByInstitutionCode(String institutionCode);
+    Optional<Institution> findByEmail(String email);
+    Optional<Institution> findByFabricMspId(String fabricMspId);
+    boolean existsByInstitutionCode(String institutionCode);
+    boolean existsByEmail(String email);
+    List<Institution> findByStatus(Institution.InstitutionStatus status);
+    List<Institution> findByType(Institution.InstitutionType type);
 }
