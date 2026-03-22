6e1b18e7fb3080ed575abc64b4f43dd996aff381
KabeloMashapa <mashapagesler03@gmail.com>
2026-03-22 12:01:53 +0200

    Adding DocumentRepository function

--- src/main/java/com/kyc_ledger/demo/repository/DocumentRepository.java
+++ src/main/java/com/kyc_ledger/demo/repository/DocumentRepository.java
@@ -1,11 +1,21 @@
 package com.kyc_ledger.demo.repository;
+import com.kyc_ledger.demo.model.Document;
 import com.kyc_ledger.demo.model.KycRecord;
 import com.kyc_ledger.demo.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import java.time.LocalDateTime;
+import java.util.List;
 import java.util.Optional;
-public interface DocumentRepository extends JpaRepository<KycRecord,Long> {
+public interface DocumentRepository extends JpaRepository<Document,Long> {
 
-    Optional<KycRecord> findByUserRecord(String user);
+    List<Document> findByKycRecord(KycRecord kycRecord);
+    List<Document> findByKycRecordAndDocumentType(
+            KycRecord kycRecord,
+            Document.DocumentType documentType);
+    Optional<Document> findByFileHash (String fileHash);
+    boolean existsByFileHash (String fileHash);
+    List<Document> findByDocumentStatus(Document.DocumentStatus status);
+
+
 }
