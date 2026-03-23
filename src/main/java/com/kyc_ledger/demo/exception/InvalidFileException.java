027478a34d2c94b719bfa607abdd429b35d28c61
KabeloMashapa <mashapagseler03@gmail.com>
2026-03-23 09:32:54 +0200

    Updating the Invalid file exception function

---
+++ src/main/java/com/kyc_ledger/demo/exception/InvalidFileException.java
@@ -0,0 +1,8 @@
+package com.kyc_ledger.demo.exception;
+
+public class InvalidFileException extends RuntimeException {
+
+    public InvalidFileException(String message) {
+        super(message);
+    }
+}
