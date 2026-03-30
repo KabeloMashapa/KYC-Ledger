a9df8f2f730eb128f352f67aab0bdd1f2bb083f9
KabeloMashapa <mashapagseler03@gmail.com>
2026-03-30 18:18:09 +0200

    Adding the get kycId get request

---
+++ src/main/java/com/kyc_ledger/demo/controller/KycController.java
@@ -0,0 +1,39 @@
+package com.kyc_ledger.demo.controller;
+import com.kyc_ledger.demo.dto.KycRequestDTO;
+import com.kyc_ledger.demo.dto.KycResponseDTO;
+import com.kyc_ledger.demo.dto.ApiResponseDTO;
+import com.kyc_ledger.demo.service.KycService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/kyc")
+@RequiredArgsConstructor
+
+public class KycController {
+
+    private final KycService kycService;
+    // POST /api/kyc/submit
+    @PostMapping("/submit")
+    @PreAuthorize("hasRole('USER')")
+    public ResponseEntity<ApiResponseDTO<KycResponseDTO>> submitKyc(
+            @Valid @ResponseBody KycRequestDTO request
+    ) {
+        return ResponseEntity.ok(kycService.submitKyc(request));
+    }
+    // GET /api/kyc/{kycId}
+    @GetMapping("/{kycId}")
+    @PreAuthorize("hasRole('USER','ADMIN','INSTITUTION')")
+    public ResponseEntity<ApiResponseDTO<KycResponseDTO>> getKycId(
+            @PathVariable String kycId
+    ) {
+        return ResponseEntity.ok(kycService.getKycById(kycId));
+    }
+
+
+
+}
