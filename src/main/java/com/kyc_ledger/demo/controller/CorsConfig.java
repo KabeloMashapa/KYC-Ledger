effb69d6ea1357d6831e0d8376efd749ac971426
KabeloMashapa <mashapagesler03@gmail.com>
2026-03-30 20:15:00 +0200

    Adding the Cors Configuration to allow the backend to communicate with the frontend

---
+++ src/main/java/com/kyc_ledger/demo/controller/CorsConfig.java
@@ -0,0 +1,33 @@
+package com.kyc_ledger.demo.controller;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+import java.util.Arrays;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public CorsFilter corsFilter() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.setAllowedOrigins(Arrays.asList(
+                "http://localhost:3000",// React frontend
+                "http://localhost:4300",// Angular frontend
+                "http:/localhost:8080" // same server
+        ));
+        config.setAllowedHeaders(Arrays.asList(
+                "Authorization",
+                "Content-Type",
+                "Accept"
+        ));
+        config.setAllowedMethods(Arrays.asList(
+                "GET","POST","PUT","DELETE","OPTIONS"
+                )
+        );
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/api/**",config);
+    }
+}
