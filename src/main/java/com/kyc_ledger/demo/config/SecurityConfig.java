2a3d3638c4013fc75bf21a2a935fa256beba3095
KabeloMashapa <mashapagesler03@gmail.com>
2026-04-02 14:30:17 +0200

    adding the security config class

---
+++ src/main/java/com/kyc_ledger/demo/config/SecurityConfig.java
@@ -0,0 +1,16 @@
+package com.kyc_ledger.demo.config;
+import com.kyc_ledger.demo.security.UserDetailsServiceImpl;
+import com.kyc_ledger.demo.security.JwtTokenProvider;
+import com.kyc_ledger.demo.security.JwtAuthFilter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.
+public class SecurityConfig {
+}
