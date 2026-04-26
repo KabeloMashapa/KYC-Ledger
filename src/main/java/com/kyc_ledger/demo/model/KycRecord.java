6f85e69857a3cbe90d00320343efcebfd5a77ba6
KabeloMashapa <mashapagesler03@gmail.com>
2026-04-26 12:10:44 +0200

    removing lombok in KycRecord class replacing with constructor and getters and setters

--- src/main/java/com/kyc_ledger/demo/model/KycRecord.java
+++ src/main/java/com/kyc_ledger/demo/model/KycRecord.java
@@ -1,14 +1,8 @@
 package com.kyc_ledger.demo.model;
 import jakarta.persistence.*;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-import lombok.AllArgsConstructor;
 import java.time.LocalDateTime;
 import java.util.List;
 
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
 @Entity
 @Table(name= "kyc_records")
 
@@ -59,6 +53,80 @@ public class KycRecord {
     @OneToMany(mappedBy = "kycRecord", cascade =  CascadeType.ALL)
     private List<Document> documents ;
 
+    public KycRecord(){
+
+    }
+    public KycRecord(Long id, String kycId, User user, String firstName,
+                     String lastName, String dateOfBirth,String nationality,
+                     String idNumber, String address, String city, String country,
+                     String blockchainTxId, String blockchainHash, KycStatus status,
+                     String rejectionReason, LocalDateTime submittedAt,
+                     LocalDateTime reviewedAt, LocalDateTime expiresAt,
+                     User reviewedBy, List<Document> documents) {
+        this.id = id;
+        this.kycId = kycId;
+        this.user = user ;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.dateOfBirth = dateOfBirth;
+        this.nationality = nationality;
+        this.idNumber = idNumber;
+        this.address = address;
+        this.city = city;
+        this.country = country;
+        this.blockchainTxId = blockchainTxId;
+        this.blockchainHash = blockchainHash;
+        this.status = status;
+        this.rejectionReason =rejectionReason;
+        this.submittedAt = submittedAt;
+        this.reviewedAt = reviewedAt;
+        this.expiresAt = expiresAt;
+        this.reviewedBy = reviewedBy;
+        this.documents = documents;
+    }
+    public Long getId() { return id;}
+    public String getKycId() { return kycId;}
+    public User getUser() { return user; }
+    public String getFirstName() { return firstName;}
+    public String getLastName() { return lastName;}
+    public String getDateOfBirth() { return dateOfBirth; }
+    public String getNationality() { return nationality; }
+    public String getIdNumber() { return idNumber; }
+    public String getAddress() { return address; }
+    public String getCity() { return city;}
+    public String getCountry() { return country;}
+    public String getBlockchainTxId() { return blockchainTxId; }
+    public String getBlockchainHash() { return blockchainHash; }
+    public KycStatus getStatus() { return status; }
+    public String getRejectionReason() { return rejectionReason; }
+    public LocalDateTime getSubmittedAt() { return submittedAt; }
+    public LocalDateTime getReviewedAt() { return reviewedAt; }
+    public LocalDateTime getExpiresAt() { return expiresAt; }
+    public User getReviewedBy() { return reviewedBy; }
+    public List<Document> getDocuments() { return documents; }
+
+    public void setId(Long id) { this.id = id; }
+    public void setKycId(String kycId) { this.kycId = kycId; }
+    public void setUser(User user) { this.user = user; }
+    public void setFirstName(String firstName) { this.firstName = firstName; }
+    public void setLastName(String lastName) { this.lastName = lastName; }
+    public void setDateOfBirth(String dateOfBirth) { this.dateOfBirth = dateOfBirth; }
+    public void setNationality(String nationality) { this.nationality = nationality; }
+    public void setIdNumber(String idNumber) { this.idNumber = idNumber; }
+    public void setAddress(String address) { this.address = address; }
+    public void setCity(String city) { this.city = city; }
+    public void setCountry(String country) { this.country = country; }
+    public void setBlockchainTxId(String blockchainTxId) { this.blockchainTxId = blockchainTxId; }
+    public void setBlockchainHash(String blockchainHash) { this.blockchainHash = blockchainHash; }
+    public void setStatus(KycStatus status) { this.status = status; }
+    public void setRejectionReason(String rejectionReason) { this.rejectionReason = rejectionReason; }
+    public void setSubmittedAt(LocalDateTime submittedAt) { this.submittedAt = submittedAt; }
+    public void setReviewedAt(LocalDateTime reviewedAt) { this.reviewedAt = reviewedAt; }
+    public void setExpiresAt(LocalDateTime expiresAt) { this.expiresAt = expiresAt; }
+    public void setReviewedBy(User reviewedBy) { this.reviewedBy = reviewedBy; }
+    public void setDocuments(List<Document> documents) { this.documents = documents; }
+
+
     @PrePersist
     protected void onCreate() {
         submittedAt = LocalDateTime.now();
@@ -67,8 +135,6 @@ public class KycRecord {
     public enum KycStatus {
         PENDING,UNDER_REVIEW,APPROVED,REJECTED,EXPIRED
     }
-
-
 
 
 }
--- src/main/java/com/kyc_ledger/demo/model/KycVerificationRequest.java
+++ src/main/java/com/kyc_ledger/demo/model/KycVerificationRequest.java
@@ -1,17 +1,42 @@
 package com.kyc_ledger.demo.model;
 import jakarta.persistence.*;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-import lombok.AllArgsConstructor;
 import java.time.LocalDateTime;
 
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
 @Entity
 @Table(name = "kyc_verification_requests")
 public class KycVerificationRequest {
 
+    public KycVerificationRequest(Long id, Institution institution,
+                                  KycRecord kycRecord, VerificationStatus status,
+                                  String blockchainTxId, String purpose,
+                                  LocalDateTime requestedAt,
+                                  LocalDateTime respondedAt) {
+        this.id = id;
+        this.institution = institution;
+        this.kycRecord = kycRecord;
+        this.status = status;
+        this.blockchainTxId = blockchainTxId;
+        this.purpose = purpose;
+        this.requestedAt = requestedAt;
+        this.respondedAt = respondedAt;
+    }
+    public Long getId() { return id; }
+    public Institution getInstitution() { return institution; }
+    public KycRecord getKycRecord() { return kycRecord; }
+    public VerificationStatus getStatus() { return status; }
+    public String getBlockchainTxId() { return blockchainTxId; }
+    public String getPurpose() { return purpose; }
+    public LocalDateTime getRequestedAt() { return requestedAt; }
+    public LocalDateTime getRespondedAt() { return respondedAt; }
+
+    public void setId(Long id) { this.id = id; }
+    public void setInstitution(Institution institution) { this.institution = institution; }
+    public void setKycRecord(KycRecord kycRecord) { this.kycRecord = kycRecord; }
+    public void setStatus(VerificationStatus status) { this.status = status; }
+    public void setBlockchainTxId(String blockchainTxId) { this.blockchainTxId = blockchainTxId; }
+    public void setPurpose(String purpose) { this.purpose = purpose; }
+    public void setRequestedAt(LocalDateTime requestedAt) { this.requestedAt = requestedAt; }
+    public void setRespondedAt(LocalDateTime respondedAt) { this.respondedAt = respondedAt; }
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
