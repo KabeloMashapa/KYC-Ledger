51f2495d5dd4ce004632c7148f6a7b0cc801db0f
KabeloMashapa <mashapagesler03@gmail.com>
2026-04-27 10:59:07 +0200

    adding getters and setters in the DocumentDTO class

--- src/main/java/com/kyc_ledger/demo/dto/DocumentDTO.java
+++ src/main/java/com/kyc_ledger/demo/dto/DocumentDTO.java
@@ -1,12 +1,7 @@
 package com.kyc_ledger.demo.dto;
-import lombok.Data;
-import lombok.AllArgsConstructor;
-import lombok.NoArgsConstructor;
 import java.time.LocalDateTime;
 
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
+
 public class DocumentDTO {
 
     private Long id;
@@ -17,4 +12,68 @@ public class DocumentDTO {
     private Long fileSize;
     private String documentStatus;      // UPLOADED, VERIFIED, REJECTED
     private LocalDateTime uploadedAt;
+
+    public DocumentDTO(Long id,String documentType,String fileName, String fileHash,
+                       String mimeType, Long fileSize, String documentStatus,
+                       LocalDateTime uploadedAt) {
+        this.id = id;
+        this.documentType = documentType;
+        this.fileName = fileName;
+        this.fileHash = fileHash;
+        this.mimeType = mimeType;
+        this.fileSize = fileSize;
+        this.documentStatus = documentStatus;
+        this.uploadedAt = uploadedAt;
+    }
+    public DocumentDTO() {}
+
+    public Long getId() {
+        return id;
+    }
+    public String getDocumentType() {
+        return documentType;
+    }
+    public String getFileName() {
+        return fileName;
+    }
+    public String getFileHash() {
+        return fileHash;
+    }
+    public String getMimeType() {
+        return mimeType;
+    }
+    public Long getFileSize() {
+        return fileSize;
+    }
+    public String getDocumentStatus() {
+        return documentStatus;
+    }
+    public LocalDateTime getUploadedAt() {
+        return uploadedAt;
+    }
+
+    public void setId(Long id){
+        this.id = id;
+    }
+    public void setDocumentType(String documentType) {
+        this.documentType = documentType;
+    }
+    public void setFileName(String fileName) {
+        this.fileName = fileName;
+    }
+    public void setFileHash(String fileHash) {
+        this.fileHash = fileHash;
+    }
+    public void setMimeType(String mimeType) {
+        this.mimeType = mimeType;
+    }
+    public void setFileSize(Long fileSize) {
+        this.fileSize = fileSize;
+    }
+    public void setDocumentStatus(String documentStatus) {
+        this.documentStatus = documentStatus;
+    }
+    public void setUploadedAt(LocalDateTime uploadedAt) {
+        this.uploadedAt = uploadedAt;
+    }
 }
