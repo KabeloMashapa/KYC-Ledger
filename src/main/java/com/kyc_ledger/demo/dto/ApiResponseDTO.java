8fbaa11064896b9d74048fe3d6f8e1ee9b9799ed
KabeloMashapa <mashapagesler03@gmail.com>
2026-04-27 10:31:32 +0200

    removing lombok imports

--- src/main/java/com/kyc_ledger/demo/dto/ApiResponseDTO.java
+++ src/main/java/com/kyc_ledger/demo/dto/ApiResponseDTO.java
@@ -1,18 +1,32 @@
 package com.kyc_ledger.demo.dto;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-import lombok.AllArgsConstructor;
 import java.time.LocalDateTime;
 
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
 public class ApiResponseDTO<T> {
 
     private boolean success;
     private String message;
     private T data;
     private LocalDateTime timestamp;
+
+    public ApiResponseDTO() {}
+
+    public ApiResponseDTO(boolean success, String message, T data,
+                          LocalDateTime timestamp) {
+        this.success = success;
+        this.message = message;
+        this.data = data;
+        this.timestamp = timestamp;
+    }
+    public boolean isSuccess() { return success; }
+    public String getMessage() { return message; }
+    public T getData() { return data; }
+    public LocalDateTime getTimestamp() {return timestamp; }
+
+    public void setSuccess(boolean success) { this.success = success ;}
+    public void setMessage(String message) { this.message = message;}
+    public void setData(T data) { this.data = data ;}
+    public void setTimestamp(LocalDateTime timestamp) { this.timestamp = timestamp;}
+
 
     public static <T> ApiResponseDTO<T> success (String message, T data) {
         ApiResponseDTO<T> response = new ApiResponseDTO<>();
